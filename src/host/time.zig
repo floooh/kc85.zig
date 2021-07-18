e963aee3e957dcc1ad6fbed2fa8047720fd5f53d
Andre Weissflog <floooh@gmail.com>
2021-07-18 17:12:04 +0200

    fix zero-duration bug in time.zig

--- src/host/time.zig
+++ src/host/time.zig
@@ -4,24 +4,27 @@
 const stm = @import("sokol").time;
 
 const state = struct {
-    var cur_time: f64 = 0;
+    var cur_frame_time: f64 = 0;
     var last_time_stamp: u64 = 0;
 };
 
 pub fn setup() void {
     // setup sokol-time
     stm.setup();
-    state.cur_time = 0;
+    state.cur_frame_time = 0;
     state.last_time_stamp = stm.now();
 }
 
 // return frame time in microseconds
-pub fn frameTime() u64 {
-    state.cur_time = stm.us(stm.roundToCommonRefreshRate(stm.laptime(&state.last_time_stamp)));
+pub fn frameTime() u32 {
+    state.cur_frame_time = stm.us(stm.roundToCommonRefreshRate(stm.laptime(&state.last_time_stamp)));
     // prevent death spiral on host systems that are too slow to
     // run the emulator in realtime, or during long frame (e.g. debugging)
-    if (state.cur_time > 24000) {
-        state.cur_time = 24000;
+    if (state.cur_frame_time < 1000) {
+        state.cur_frame_time = 1000;
     }
-    return @floatToInt(u64, state.cur_time);
+    if (state.cur_frame_time > 24000) {
+        state.cur_frame_time = 24000;
+    }
+    return @floatToInt(u32, state.cur_frame_time);
 }
