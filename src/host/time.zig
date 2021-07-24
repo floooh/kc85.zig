7ab8e21e5124f0f3b4310bdcb64ee018146d25c0
Andre Weissflog <floooh@gmail.com>
2021-07-24 18:06:29 +0200

    time.zig: add an elapsed() helper function

--- src/host/time.zig
+++ src/host/time.zig
@@ -6,13 +6,15 @@ const stm = @import("sokol").time;
 const state = struct {
     var cur_frame_time: f64 = 0;
     var last_time_stamp: u64 = 0;
+    var start_time: u64 = 0;
 };
 
 pub fn setup() void {
     // setup sokol-time
     stm.setup();
     state.cur_frame_time = 0;
-    state.last_time_stamp = stm.now();
+    state.start_time = stm.now();
+    state.last_time_stamp = state.start_time;
 }
 
 // return frame time in microseconds
@@ -27,4 +29,9 @@ pub fn frameTime() u32 {
         state.cur_frame_time = 24000;
     }
     return @floatToInt(u32, state.cur_frame_time);
+}
+
+pub fn elapsed(micro_seconds: u64) bool {
+    const elapsed_us = @floatToInt(u64, stm.us(stm.since(state.start_time)));
+    return elapsed_us > micro_seconds;
 }
